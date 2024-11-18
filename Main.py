559bb47b710d92e706ebca216e78449febc2fc4f
ARS <lyrdasyd266@gmail.com>
2024-11-19 01:06:16 +0330

    Add Installer

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from print_color import print
 import os
 import sys
---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt update && apt upgrade -y
+sudo apt install pip
+sudo apt install git
+git clone "https://github.com/ARS-83/v2ray-ConfigGenerator.git"
+current_folder=$(basename "$PWD")
+cd "/$current_folder/v2ray-ConfigGenerator"
+echo "$PWD"
+pip install -r requirements.txt
+chmod +x Main.py
+sudo ln -s /$current_folder/v2ray-ConfigGenerator/Main.py /usr/local/bin/generator
